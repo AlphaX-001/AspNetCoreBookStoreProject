872b1f3f93f8a2d1d1a40b25e18d92d38304e153
Anith Bairagi <anithbairagi8@gmail.com>
2022-06-27 03:19:35 +0530

    Added Layout

--- BookProject/Program.cs
+++ BookProject/Program.cs
@@ -7,6 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+#if DEBUG
+    builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+#endif
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
